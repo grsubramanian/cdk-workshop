f7838a04c429ba5c9ade4171b9924c89044b36e9
Gokul Ramanan Subramanian <gokul.r.subramanian@gmail.com>
2022-08-01 18:42:53 +0100

    Implement Lambda and API gateway

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import { ApiGateway } from 'aws-cdk-lib/aws-events-targets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Defines an AWS Lambda resource.
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    // Defines an API gateway endpoint backed by the "hello" Lambda function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
